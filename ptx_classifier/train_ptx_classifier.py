14f274516e0914cacedddd41047aabcbf72a0ed2
avielbl <avielbl@gmail.com>
2017-09-30 13:13:06 +0300

    learning curve after raising dropout to 0.5

--- ptx_classifier/train_ptx_classifier.py
+++ ptx_classifier/train_ptx_classifier.py
@@ -42,7 +42,7 @@ def get_model_for_patches():
     model.add(Conv2D(64, (3, 3), padding='same'))
     model.add(LeakyReLU(0.01))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
 
     model.add(Conv2D(128, (3, 3), padding='same'))
     model.add(LeakyReLU(0.01))
@@ -52,7 +52,7 @@ def get_model_for_patches():
 
     model.add(Dense(64))
     model.add(LeakyReLU(0.01))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.5))
 
     model.add(Dense(2))
     model.add(Activation('softmax'))
