86f449a6ba62a1283cb0eaa038eca91b7878fd1d
avielbl <avielbl@gmail.com>
2017-09-30 09:01:58 +0300

    first learning results

--- ptx_classifier/utils.py
+++ ptx_classifier/utils.py
@@ -1,7 +1,8 @@
-training_path = r'C:\projects\CXR_thesis\data_repo\TRAINING'
+# training_path = r'C:\projects\CXR_thesis\data_repo\TRAINING'
+training_path = r'D:\cxr_thesis\data_repo\TRAINING'
 
 
 im_size = 1024
 patch_sz = 32
 smooth = 1.
-max_num_of_patches = 2000000
+max_num_of_patches = 4000000
