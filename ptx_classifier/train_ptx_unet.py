a08b4f8740d55d3d12ed07b5c99814e482f439fe
avielbl <avielbl@gmail.com>
2017-10-02 09:33:48 +0300

    first skeleton implementation of the unet training script

---
+++ ptx_classifier/train_ptx_unet.py
@@ -0,0 +1,15 @@
+import numpy as np
+np.random.seed(1)
+
+from aid_funcs.keraswrapper import get_unet, load_model, plot_first_layer
+from utils import *
+
+def prep_data():
+    pass
+
+def train_model(db):
+    pass
+
+if __name__ == '__main__':
+    db = prep_data()
+    model = train_model(db)
