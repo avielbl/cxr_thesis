af306424dc3e4565918f01fe99a18ccbeedc5e3e
avibenco <avibenco@gmail.com>
2017-10-02 23:19:59 +0300

    working version of the validation script for the patch based model

--- ptx_classifier/validate_model.py
+++ ptx_classifier/validate_model.py
@@ -1,25 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pickle
 import os
-import keras
 from sklearn.metrics import roc_curve, auc
 
 from aid_funcs.keraswrapper import load_model
+from misc import load_from_h5
 from utils import *
 
-val_data_patches = pickle.load(open(os.path.join(training_path, 'val_patches.pkl'), 'rb'))
-val_data_labels = pickle.load(open(os.path.join(training_path, 'val_labels.pkl'), 'rb'))
+val_data_labels = load_from_h5(os.path.join(training_path, 'val_labels.h5'))
+val_data_patches = load_from_h5(os.path.join(training_path, 'val_patches.h5'))
 
 model = load_model(model_path)
 
 nb_val = val_data_labels.shape[0]
-# scores = model.predict(val_data_patches, batch_size=1000, verbose=1)
-scores = np.zeros((nb_val,), dtype=np.uint8)
-for i, patch in enumerate(val_data_patches):
-    scores[i] = model.predict(np.expand_dims(patch, 0))
-    if i % 100 == 0:
-        print('Predicted {}/{}'.format(i, nb_val))
+scores = model.predict(val_data_patches, batch_size=1000, verbose=1)[:,1]
 
 # calculating ROC per pixel
 fpr, tpr, thresh = roc_curve(val_data_labels, scores)
@@ -31,7 +25,7 @@ opt_thresh = thresh[opt_ind]
 # plotting the roc
 plt.figure(1)
 plt.plot(fpr, tpr, label='ROC')
-plt.plot(fpr, thresh, label='Threshold')
+# plt.plot(fpr, thresh, label='Threshold')
 plt.plot(fpr[opt_ind], tpr[opt_ind], 'ro', label='Optimal thresh')
 plt.minorticks_on()
 plt.grid(b=True, which='both')
