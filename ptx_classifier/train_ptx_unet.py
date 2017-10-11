685fbda62f2db19e72d81039266b79425f8c7956
avielbl <avielbl@gmail.com>
2017-10-11 16:10:16 +0300

    adopted unet training script to train model for each side seperatly

--- ptx_classifier/train_ptx_unet.py
+++ ptx_classifier/train_ptx_unet.py
@@ -1,77 +1,172 @@
 import numpy as np
 np.random.seed(1)
+from collections import namedtuple
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from keras.optimizers import Adam
 from sklearn.metrics import roc_curve, auc
+from skimage import measure
+from scipy.misc import imread
 
 from aid_funcs import image
-
+from aid_funcs.misc import zip_save, save_to_h5, load_from_h5
 from aid_funcs.keraswrapper import get_unet, load_model, plot_first_layer, PlotLearningCurves
+from lung_seg.utilfuncs import seperate_lungs
+from aid_funcs import CXRLoadNPrep as clp
 from utils import *
 
-train_data_lst, val_data_lst = train_val_partition()
+Case = namedtuple('Case', ['left', 'right'])
+Data = namedtuple('Case', ['img', 'lung_mask', 'ptx_mask'])
 
 
-def prep_data():
-    nb_train = len(train_data_lst)
-    nb_val = len(val_data_lst)
-    train_imgs_arr = np.zeros((nb_train, 1, im_size, im_size))
-    train_masks_arr = np.zeros((nb_train, 1, im_size, im_size), dtype=np.uint8)
-    val_imgs_arr = np.zeros((nb_val, 1, im_size, im_size))
-    val_masks_arr = np.zeros((nb_val, 1, im_size, im_size), dtype=np.uint8)
-    for i in range(nb_train):
-        train_imgs_arr[i] = image.imresize(train_data_lst[i]['img'], (im_size, im_size))
-        if train_data_lst[i]['ptx_mask'] is None:
-            ptx_mask = np.zeros((im_size, im_size), dtype=np.uint8)
+def generate_data_lst():
+
+    lung_seg_path = os.path.join(training_path, 'lung_seg_gt')
+    ptx_masks_path = os.path.join(training_path, 'ptx_masks_gt')
+    imgs_path_lst = create_imgs_path_lst()
+    train_set_lst = []
+
+    for im_count, curr_img_path in enumerate(imgs_path_lst):
+        img_name = os.path.split(curr_img_path)[1][:-4]
+        img = clp.load_dicom(curr_img_path)
+        img = image.square_image(img)
+        lung_path = os.path.join(lung_seg_path, img_name + '.png')
+        if os.path.isfile(lung_path):
+            # Loading lung mask and change its size to match img size
+            lungs_mask = imread(lung_path, mode='L')
+            lungs_mask = image.imresize(lungs_mask, img.shape)
+
+            # Loading ptx mask if exist and change its size to match img size
+            ptx_path = os.path.join(ptx_masks_path, img_name + '.png')
+            if os.path.isfile(ptx_path):
+                ptx_mask = imread(ptx_path, mode='L')
+                ptx_mask = image.imresize(ptx_mask, img.shape)
+                ptx_mask[lungs_mask == 0] = 0
+            else:
+                ptx_mask = None
+
+            case = get_lungs_bb(img, lungs_mask, ptx_mask)
+            case = normalize_case(case)
+            train_set_lst.append(case)
+        print("Loaded image number %i" % im_count)
+    return train_data_lst
+
+def create_imgs_path_lst():
+    # Creating list of images' paths
+    pos_path = os.path.join(training_path, 'pos_cases')
+    neg_path = os.path.join(training_path, 'neg_cases')
+    pos_files = [os.path.join(pos_path, file) for file in os.listdir(pos_path)]
+    neg_files = [os.path.join(neg_path, file) for file in os.listdir(neg_path)]
+    return pos_files + neg_files
+
+def get_lungs_bb(img, lungs_mask, ptx_mask=None):
+    def get_lung_bb(img, lung_mask, ptx_mask=None):
+        lung_bbox = measure.regionprops(lung_mask.astype(np.uint8))
+        lung_bbox = lung_bbox[0].bbox
+        img = img[lung_bbox[0]:lung_bbox[2], lung_bbox[1]:lung_bbox[3]]
+        lung_mask = lung_mask[lung_bbox[0]:lung_bbox[2], lung_bbox[1]:lung_bbox[3]]
+        if ptx_mask is not None:
+            ptx_mask = ptx_mask[lung_bbox[0]:lung_bbox[2], lung_bbox[1]:lung_bbox[3]]
+        return Data(img, lung_mask, ptx_mask)
+
+    lung_map_dilated = image.safe_binary_morphology(lungs_mask, sesize=15, mode='dilate')
+    sep_lung_masks = seperate_lungs(lung_map_dilated)
+    left = get_lung_bb(img, sep_lung_masks.l_lung_mask, ptx_mask)
+    right = get_lung_bb(img, sep_lung_masks.r_lung_mask, ptx_mask)
+    return Case(left, right)
+
+
+def normalize_case(case):
+    def resize_side(side):
+        # Resizing cropped image and masks for getting same scale for all images
+        img = image.imresize(side.img.astype(np.float32), (im_size, im_size))
+        lung_mask = image.imresize(side.lung_mask.astype(np.float32), (im_size, im_size)).astype(np.uint8)
+        if side.ptx_mask is not None:
+            ptx_mask = image.imresize(side.ptx_mask.astype(np.float32), (im_size, im_size)).astype(np.uint8)
         else:
-            ptx_mask = image.imresize(train_data_lst[i]['ptx_mask'], (im_size, im_size))
-        ptx_mask[ptx_mask > 0] = 1
-        train_masks_arr[i] = ptx_mask
-    for i in range(nb_val):
-        val_imgs_arr[i] = image.imresize(val_data_lst[i]['img'], (im_size, im_size))
-        if val_data_lst[i]['ptx_mask'] is None:
-            ptx_mask = np.zeros((im_size, im_size), dtype=np.uint8)
-        else:
-            ptx_mask = image.imresize(val_data_lst[i]['ptx_mask'], (im_size, im_size))
-        ptx_mask[ptx_mask > 0] = 1
-        val_masks_arr[i] = ptx_mask
+            ptx_mask = None
+        return Data(img, lung_mask, ptx_mask)
+
+    def normalize_img(img, lung_mask):
+        # Normalizing each image based on mean and std of lung pixels
+        nan_img = img.copy()
+        nan_img = nan_img.astype(np.float32)
+        nan_img[lung_mask == 0] = np.nan
+        mean_val = np.nanmean(nan_img)
+        std_val = np.nanstd(nan_img)
+        out_img = img.copy().astype(np.float32)
+        out_img -= mean_val
+        out_img /= std_val
+        out_img[np.isnan(out_img)] = np.nanmax(out_img)
+        return out_img
+
+    left = resize_side(case.left)
+    right = resize_side(case.right)
+    l_img = normalize_img(left.img, left.lung_mask)
+    r_img = normalize_img(right.img, right.lung_mask)
+    return Case(Data(l_img, left.lung_mask, left.ptx_mask), Data(r_img, right.lung_mask, right.ptx_mask))
+
+
+def prep_data(train_data_lst, val_data_lst, side_str):
+    def prep_set(set):
+        n = len(set)
+        imgs_arr = np.zeros((n, 1, im_size, im_size))
+        masks_arr = np.zeros((n, 1, im_size, im_size))
+        for i in range(n):
+            if side_str == 'left':
+                case = set[i].left
+            elif side_str == 'right':
+                case = set[i].right
+            else:
+                raise ValueError()
+
+            imgs_arr[i] = image.imresize(case.img, (im_size, im_size))
+            if case.ptx_mask is None:
+                ptx_mask = np.zeros((im_size, im_size), dtype=np.uint8)
+            else:
+                ptx_mask = image.imresize(case.ptx_mask, (im_size, im_size))
+            ptx_mask[ptx_mask > 0] = 1
+            masks_arr[i] = ptx_mask
+        return imgs_arr, masks_arr
+
+    train_imgs_arr, train_masks_arr = prep_set(train_data_lst)
+    val_imgs_arr, val_masks_arr = prep_set(val_data_lst)
     db = (train_imgs_arr, train_masks_arr, val_imgs_arr, val_masks_arr)
     return db
-
-def get_lung_masks(data_lst):
-    nb_images = len(data_lst)
-    lung_masks_arr = np.zeros((nb_images, 1, im_size, im_size), dtype=np.uint8)
-    for i, case in enumerate(data_lst):
-        lung_masks_arr[i] = image.imresize(case['lung_mask'], (im_size, im_size))
-    lung_masks_arr[lung_masks_arr > 0] = 1
-    return lung_masks_arr
-
-def augment_data(db):
-    pass
-
-def analyze_performance(db):
-    model_path = 'ptx_model_unet' + '.hdf5'
-    model = load_model(model_path, custom_objects='dice_coef_loss')
-    lung_masks_arr = get_lung_masks(val_data_lst)
-    ptx_pred = model.predict(db[2], batch_size=10, verbose=1)
-    ptx_pred *= lung_masks_arr
-    # calculating ROC per pixel
-    fpr, tpr, thresh = roc_curve(db[3].flatten(), ptx_pred.flatten())
-    roc_auc = auc(fpr, tpr)
-    dist_to_opt = np.sqrt(fpr ** 2 + (1 - tpr) ** 2)
-    opt_ind = np.argmin(dist_to_opt)
-    opt_thresh = thresh[opt_ind]
-
-    # plotting the roc
-    plt.figure(1)
-    plt.plot(fpr, tpr, label='ROC')
-    # plt.plot(fpr, thresh, label='Threshold')
-    plt.plot(fpr[opt_ind], tpr[opt_ind], 'ro', label='Optimal thresh')
-    plt.minorticks_on()
-    plt.grid(b=True, which='both')
-    plt.legend(loc='upper right')
-    plt.title('ROC curve (area = %0.2f, opt thresh = %0.2f)' % (100 * roc_auc, opt_thresh))
-    plt.savefig('roc analysis unet.png')
+#TODO: update commented 2 functions bellow to fit partition to 2 lungs
+# def get_lung_masks(data_lst):
+#     nb_images = len(data_lst)
+#     lung_masks_arr = np.zeros((nb_images, 1, im_size, im_size), dtype=np.uint8)
+#     for i, case in enumerate(data_lst):
+#         lung_masks_arr[i] = image.imresize(case['lung_mask'], (im_size, im_size))
+#     lung_masks_arr[lung_masks_arr > 0] = 1
+#     return lung_masks_arr
+#
+# def augment_data(db):
+#     pass
+#
+# def analyze_performance(db):
+#     model_path = 'ptx_model_unet' + '.hdf5'
+#     model = load_model(model_path, custom_objects='dice_coef_loss')
+#     lung_masks_arr = get_lung_masks(val_data_lst)
+#     ptx_pred = model.predict(db[2], batch_size=10, verbose=1)
+#     ptx_pred *= lung_masks_arr
+#     # calculating ROC per pixel
+#     fpr, tpr, thresh = roc_curve(db[3].flatten(), ptx_pred.flatten())
+#     roc_auc = auc(fpr, tpr)
+#     dist_to_opt = np.sqrt(fpr ** 2 + (1 - tpr) ** 2)
+#     opt_ind = np.argmin(dist_to_opt)
+#     opt_thresh = thresh[opt_ind]
+#
+#     # plotting the roc
+#     plt.figure(1)
+#     plt.plot(fpr, tpr, label='ROC')
+#     # plt.plot(fpr, thresh, label='Threshold')
+#     plt.plot(fpr[opt_ind], tpr[opt_ind], 'ro', label='Optimal thresh')
+#     plt.minorticks_on()
+#     plt.grid(b=True, which='both')
+#     plt.legend(loc='upper right')
+#     plt.title('ROC curve (area = %0.2f, opt thresh = %0.2f)' % (100 * roc_auc, opt_thresh))
+#     plt.savefig('roc analysis unet.png')
 
 def train_model(db):
     lr = 0.0001
@@ -91,8 +186,14 @@ def train_model(db):
               verbose=1, shuffle=True,
               callbacks=callbacks)
 
+##########################################################
+###########            MAIN SCRIPT        ################
+##########################################################
 
-if __name__ == '__main__':
-    db = prep_data()
-    model = train_model(db)
-    analyze_performance(db)
+
+train_data_lst, val_data_lst = train_val_partition(generate_data_lst())
+db_left = prep_data(train_data_lst, val_data_lst, 'left')
+save_to_h5(db_left, os.path.join(training_path, 'db_left.h5'))
+# db_left = load_from_h5(os.path.join(training_path, 'db_left.h5'))
+model_left = train_model(db_left)
+# analyze_performance(db)
