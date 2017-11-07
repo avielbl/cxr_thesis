f5c16b4a0a01edd36db605ef635a7c30a8221e2e
avibenco <avibenco@gmail.com>
2017-11-07 15:18:48 +0200

    trained model of entire image using unet with dice loss function

--- ptx_classifier/batch_predict_unet.py
+++ ptx_classifier/batch_predict_unet.py
@@ -1,51 +1,49 @@
-
 from aid_funcs.keraswrapper import load_model
 from misc import load_from_h5, roc_plotter
 from prep_data_for_unet import prep_set
 from utils import *
 
 
-def get_lung_masks(data_lst):
-    nb_images = len(data_lst)
-    lung_masks_arr = np.zeros((nb_images, 1, im_size, im_size), dtype=np.uint8)
-    for i, case in enumerate(data_lst):
-        lung_masks_arr[i] = image.imresize(case.lung_mask, (im_size, im_size))
-    lung_masks_arr[lung_masks_arr > 0] = 1
-    return lung_masks_arr
 
+def analyze_performance(model=None, val_data = None, model_name=''):
+    if model is None:
+        # model = load_model('ptx_model_unet.hdf5')
+        model = load_model('ptx_model_unet.hdf5', custom_objects='dice_coef_loss')
+    if val_data is None:
+        _, val_data_lst = process_and_augment_data()
+        val_imgs_arr, val_masks_arr = prep_set(val_data_lst)
+    else:
+        val_imgs_arr = val_data[0]
+        val_masks_arr = val_data[1]
 
-model = load_model('ptx_model_unet.hdf5')
+    lung_masks_arr = load_from_h5(os.path.join(training_path, 'db_lung_masks_arr.h5'))
 
+    scores = model.predict(val_imgs_arr, batch_size=5, verbose=1)
 
-_, val_data_lst = process_and_augment_data()
-val_imgs_arr, val_masks_arr = prep_set(val_data_lst)
-lung_masks_arr = get_lung_masks(val_data_lst)
-scores = model.predict(val_imgs_arr, batch_size=5, verbose=1)
+    # calculating ROC per pixel
+    lung_flatten = np.array(lung_masks_arr).flatten()
+    lung_idx = np.where(lung_flatten > 0)
+    scores_vec = scores.flatten()
+    scores_vec = scores_vec[lung_idx]
+    gt_vec = np.uint8(val_masks_arr.flatten())
+    gt_vec = gt_vec[lung_idx]
+    roc_plotter(gt_vec, scores_vec, model_name)
 
-# calculating ROC per pixel
-lung_flatten = lung_masks_arr.flatten()
-lung_idx = np.where(lung_flatten > 0)
-scores_vec = scores.flatten()
-scores_vec = scores_vec[lung_idx]
-gt_vec = np.uint8(val_masks_arr.flatten())
-gt_vec = gt_vec[lung_idx]
-roc_plotter(gt_vec, scores_vec, 'U-Net')
+    pred_maps = np.zeros_like(scores, dtype=np.uint8)
+    pred_maps[scores > 0.11] = 255
 
-pred_maps = np.zeros_like(scores, dtype=np.uint8)
-pred_maps[scores > 0.11] = 255
+    nb_val = val_imgs_arr.shape[0] + 1
+    plt.figure()
+    if os.path.isdir('results') is False:
+        os.mkdir('results')
+    for i in range(nb_val):
+        img = np.squeeze(val_imgs_arr[i])
+        pred = np.squeeze(pred_maps[i]) * np.squeeze(lung_masks_arr[i])
 
-nb_val = val_imgs_arr.shape[0]
-plt.figure()
-if os.path.isdir('results') is False:
-    os.mkdir('results')
-for i in range(nb_val):
-    img = np.squeeze(val_imgs_arr[i])
-    pred = np.squeeze(pred_maps[i]) * np.squeeze(lung_masks_arr[i])
+        gt = np.squeeze(val_masks_arr[i])
 
-    gt = np.squeeze(val_masks_arr[i])
-
-    fig=show_image_with_overlay(img, overlay=gt, overlay2=pred)
-    plt.axis('off')
-    out_path = 'results\\' + str(i) + '.png'
-    plt.savefig(out_path, bbox_inches='tight')
-    print('saved image {}/{}'.format(i, nb_val))
+        show_image_with_overlay(img, overlay=gt, overlay2=pred)
+        plt.axis('off')
+        out_path = 'results\\' + str(i) + '.png'
+        plt.savefig(out_path, bbox_inches='tight')
+        print('saved image {}/{}'.format(i, nb_val))
--- ptx_classifier/train_ptx_unet.py
+++ ptx_classifier/train_ptx_unet.py
@@ -1,16 +1,19 @@
 import numpy as np
+from keras.losses import binary_crossentropy, categorical_crossentropy
 
 np.random.seed(1)
 from collections import namedtuple
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from keras.optimizers import Adam
+import tensorflow as tf
 from sklearn.metrics import roc_curve, auc
 from skimage import measure
 from scipy.misc import imread
 
 from aid_funcs import image
 from aid_funcs.misc import zip_save, save_to_h5, load_from_h5
-from aid_funcs.keraswrapper import get_unet, load_model, plot_first_layer, PlotLearningCurves
+from aid_funcs.keraswrapper import get_unet, load_model, plot_first_layer, PlotLearningCurves, get_class_weights, \
+    weighted_pixelwise_crossentropy, dice_coef_loss, dice_coef
 from lung_seg.utilfuncs import seperate_lungs
 from aid_funcs import CXRLoadNPrep as clp
 from utils import *
@@ -18,37 +21,38 @@ from utils import *
 Case = namedtuple('Case', ['left', 'right'])
 Data = namedtuple('Case', ['img', 'lung_mask', 'ptx_mask'])
 
+EPSILON = 1e-8
 
-def generate_data_lst():
-    lung_seg_path = os.path.join(training_path, 'lung_seg_gt')
-    ptx_masks_path = os.path.join(training_path, 'ptx_masks_gt')
-    imgs_path_lst = create_imgs_path_lst()
-    train_set_lst = []
-
-    for im_count, curr_img_path in enumerate(imgs_path_lst):
-        img_name = os.path.split(curr_img_path)[1][:-4]
-        img = clp.load_dicom(curr_img_path)
-        img = image.square_image(img)
-        lung_path = os.path.join(lung_seg_path, img_name + '.png')
-        if os.path.isfile(lung_path):
-            # Loading lung mask and change its size to match img size
-            lungs_mask = imread(lung_path, mode='L')
-            lungs_mask = image.imresize(lungs_mask, img.shape)
-
-            # Loading ptx mask if exist and change its size to match img size
-            ptx_path = os.path.join(ptx_masks_path, img_name + '.png')
-            if os.path.isfile(ptx_path):
-                ptx_mask = imread(ptx_path, mode='L')
-                ptx_mask = image.imresize(ptx_mask, img.shape)
-                ptx_mask[lungs_mask == 0] = 0
-            else:
-                ptx_mask = None
-
-            case = get_lungs_bb(img, lungs_mask, ptx_mask)
-            case = normalize_case(case)
-            train_set_lst.append(case)
-        print("Loaded image number %i" % im_count)
-    return train_data_lst
+# def generate_data_lst():
+#     lung_seg_path = os.path.join(training_path, 'lung_seg_gt')
+#     ptx_masks_path = os.path.join(training_path, 'ptx_masks_gt')
+#     imgs_path_lst = create_imgs_path_lst()
+#     train_set_lst = []
+#
+#     for im_count, curr_img_path in enumerate(imgs_path_lst):
+#         img_name = os.path.split(curr_img_path)[1][:-4]
+#         img = clp.load_dicom(curr_img_path)
+#         img = image.square_image(img)
+#         lung_path = os.path.join(lung_seg_path, img_name + '.png')
+#         if os.path.isfile(lung_path):
+#             # Loading lung mask and change its size to match img size
+#             lungs_mask = imread(lung_path, mode='L')
+#             lungs_mask = image.imresize(lungs_mask, img.shape)
+#
+#             # Loading ptx mask if exist and change its size to match img size
+#             ptx_path = os.path.join(ptx_masks_path, img_name + '.png')
+#             if os.path.isfile(ptx_path):
+#                 ptx_mask = imread(ptx_path, mode='L')
+#                 ptx_mask = image.imresize(ptx_mask, img.shape)
+#                 ptx_mask[lungs_mask == 0] = 0
+#             else:
+#                 ptx_mask = None
+#
+#             case = get_lungs_bb(img, lungs_mask, ptx_mask)
+#             case = normalize_case(case)
+#             train_set_lst.append(case)
+#         print("Loaded image number %i" % im_count)
+#     return train_data_lst
 
 
 def create_imgs_path_lst():
@@ -136,66 +140,56 @@ def prep_data(train_data_lst, val_data_lst, side_str):
     return db
 
 
-# TODO: update commented 2 functions bellow to fit partition to 2 lungs
-# def get_lung_masks(data_lst):
-#     nb_images = len(data_lst)
-#     lung_masks_arr = np.zeros((nb_images, 1, im_size, im_size), dtype=np.uint8)
-#     for i, case in enumerate(data_lst):
-#         lung_masks_arr[i] = image.imresize(case['lung_mask'], (im_size, im_size))
-#     lung_masks_arr[lung_masks_arr > 0] = 1
-#     return lung_masks_arr
-#
-# def augment_data(db):
-#     pass
-#
-# def analyze_performance(db):
-#     model_path = 'ptx_model_unet' + '.hdf5'
-#     model = load_model(model_path, custom_objects='dice_coef_loss')
-#     lung_masks_arr = get_lung_masks(val_data_lst)
-#     ptx_pred = model.predict(db[2], batch_size=10, verbose=1)
-#     ptx_pred *= lung_masks_arr
-#     # calculating ROC per pixel
-#     fpr, tpr, thresh = roc_curve(db[3].flatten(), ptx_pred.flatten())
-#     roc_auc = auc(fpr, tpr)
-#     dist_to_opt = np.sqrt(fpr ** 2 + (1 - tpr) ** 2)
-#     opt_ind = np.argmin(dist_to_opt)
-#     opt_thresh = thresh[opt_ind]
-#
-#     # plotting the roc
-#     plt.figure(1)
-#     plt.plot(fpr, tpr, label='ROC')
-#     # plt.plot(fpr, thresh, label='Threshold')
-#     plt.plot(fpr[opt_ind], tpr[opt_ind], 'ro', label='Optimal thresh')
-#     plt.minorticks_on()
-#     plt.grid(b=True, which='both')
-#     plt.legend(loc='upper right')
-#     plt.title('ROC curve (area = %0.2f, opt thresh = %0.2f)' % (100 * roc_auc, opt_thresh))
-#     plt.savefig('roc analysis unet.png')
+def get_lung_masks(data_lst):
+    nb_images = len(data_lst)
+    lung_masks_arr = np.zeros((nb_images, 1, im_size, im_size), dtype=np.uint8)
+    for i, case in enumerate(data_lst):
+        lung_masks_arr[i] = image.imresize(case.lung_mask, (im_size, im_size))
+    lung_masks_arr[lung_masks_arr > 0] = 1
+    return lung_masks_arr
 
 
-
-def train_model(db):
-    lr = 0.0001
+def train_model(db, model_name, class_weights=(1, 1)):
+    print('Building model...')
     optim_fun = Adam(lr=0.00001, decay=0.00002)
-
-    model = get_unet(im_size, filters=16, optim_fun=optim_fun)
+    # loss_fun = weighted_pixelwise_crossentropy(class_weights)
+    loss_fun = dice_coef_loss
+    metrics = dice_coef
+    model = get_unet(im_size, filters=16, optim_fun=optim_fun,
+                     loss_fun=loss_fun,
+                     metrics=dice_coef,
+                     nb_classes=1)
     model.summary()
-    model_file_name = 'ptx_model_unet' + '.hdf5'
+    model_file_name = 'ptx_model_' + model_name + '.hdf5'
 
     model_checkpoint = ModelCheckpoint(model_file_name, monitor='val_loss', mode='min', save_best_only=True, verbose=1)
-    early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=10, verbose=1)
-    reduce_lr_on_plateu = ReduceLROnPlateau(monitor='val_loss', mode='min', patience=5, factor=0.3, verbose=1)
+    early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=5, verbose=1)
+    reduce_lr_on_plateu = ReduceLROnPlateau(monitor='val_loss', mode='min', patience=3, factor=0.3, verbose=1)
+    # plot_curves_callback = PlotLearningCurves()
     plot_curves_callback = PlotLearningCurves(metric_name='dice_coef')
     callbacks = [model_checkpoint, early_stopping, reduce_lr_on_plateu, plot_curves_callback]
+    # print('Transform masks to one-hot...')
+    # db[1] = categorize(db[1])
+    # db[3] = categorize(db[3])
+    print('Start fitting...')
     model.fit(db[0], db[1], batch_size=5, epochs=100,
               validation_data=(db[2], db[3]),
               verbose=1, shuffle=True,
-              callbacks=callbacks)
+              callbacks=callbacks,
+              class_weight=class_weights)
 
+    return model
+
+def categorize(arr):
+    out = np.zeros((arr.shape[0], 2, arr.shape[2], arr.shape[3]), dtype=np.uint8)
+    out[:, 0, :, :] = (1 - arr).squeeze()
+    out[:, 1, :, :] = arr.squeeze()
+    return out
 
 ##########################################################
 ###########            MAIN SCRIPT        ################
 ##########################################################
+print('Loading data...')
 db = [
     load_from_h5(os.path.join(training_path, 'db_train_imgs_arr.h5')),
     load_from_h5(os.path.join(training_path, 'db_train_masks_arr.h5')).astype(np.uint8),
@@ -203,5 +197,14 @@ db = [
     load_from_h5(os.path.join(training_path, 'db_val_masks_arr.h5')).astype(np.uint8)
 ]
 
-model_left = train_model(db)
-# analyze_performance(db)
+# train_shape = db[1].shape
+# nb_pixels = train_shape[0] * train_shape[1] * train_shape[2] * train_shape[3]
+# nb_pos = np.sum(db[1])
+# nb_neg = nb_pixels - nb_pos
+
+# class_weights = get_class_weights(db[1])
+class_weights = None
+model_name = 'U-Net_DICE'
+# model = train_model(db, model_name, class_weights)
+from batch_predict_unet import analyze_performance
+analyze_performance(model=None, val_data=(db[2], db[3]), model_name=model_name)
