f1e4d7d17ace36444f336f8e6dc8d8f674704969
avielbl <avielbl@gmail.com>
2017-12-31 17:35:52 +0200

    Added function for training using vgg16

--- ptx_classifier/train_global_classifier.py
+++ ptx_classifier/train_global_classifier.py
@@ -9,8 +9,9 @@ import os
 import matplotlib.pyplot as plt
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from keras.layers import Conv2D, LeakyReLU, MaxPooling2D, Dropout, Flatten, Dense, Activation
-from keras.models import Sequential
+from keras.models import Sequential, Model
 from keras.optimizers import Adam
+from keras.applications.vgg16 import VGG16
 
 from image import imresize
 from misc import load_from_h5, save_to_h5
@@ -69,11 +70,29 @@ def prep_set_for_global_classifier():
     save_to_h5(val_global_label, os.path.join(training_path, 'val_global_label_arr.h5'))
 
 
+def build_model_vgg16_based(nb_epochs):
+    base_model = VGG16(weights='imagenet', include_top=False,
+                       input_shape=(im_sz, im_sz, 1))
+    x = base_model.get_layer('block4_pool').output
+    x = Flatten()(x)
+    x = Dense(64, activation='relu')(x)
+    prediction = Dense(1, activation='softmax')(x)
+    model = Model(inputs=base_model.input, outputs=prediction)
+    lr = 0.00001
+    decay_fac = 1
+    optim_fun = Adam(lr=lr, decay=decay_fac * lr / nb_epochs)
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer=optim_fun,
+                  metrics=['accuracy'])
+    return model
+
+
 def build_model(nb_epochs):
     model = Sequential()
 
-    model.add(Conv2D(16, (5, 5), padding='same', input_shape=(im_sz, im_sz, 1), kernel_initializer='he_normal'))
-    model.add(LeakyReLU(0.01))
+    model.add(Conv2D(16, (3, 3), padding='same', input_shape=(im_sz, im_sz, 1), kernel_initializer='he_normal'))
+    model.add(LeakyReLU(0.0))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Conv2D(32, (3, 3), padding='same', kernel_initializer='he_normal'))
@@ -82,8 +101,8 @@ def build_model(nb_epochs):
 
     model.add(Flatten())
 
-    model.add(Dense(128))
-    model.add(LeakyReLU(0.01))
+    model.add(Dense(64))
+    model.add(LeakyReLU(0.0))
     # model.add(Dropout(0.5))
 
     model.add(Dense(1, activation='softmax'))
@@ -97,42 +116,44 @@ def build_model(nb_epochs):
     return model
 
 
-##########################################################
-###########            MAIN SCRIPT        ################
-##########################################################
+def train_model():
+    # prep_set_for_global_classifier()
+    print('Loading data...')
+    db = [
+        load_from_h5(os.path.join(training_path, 'train_scores_maps_arr.h5')),
+        load_from_h5(os.path.join(training_path, 'train_global_label_arr.h5')),
+        load_from_h5(os.path.join(training_path, 'val_scores_maps_arr.h5')).astype(np.uint8),
+        load_from_h5(os.path.join(training_path, 'val_global_label_arr.h5')).astype(np.uint8)
+    ]
 
-prep_set_for_global_classifier()
-print('Loading data...')
-db = [
-    load_from_h5(os.path.join(training_path, 'train_scores_maps_arr.h5')),
-    load_from_h5(os.path.join(training_path, 'train_global_label_arr.h5')),
-    load_from_h5(os.path.join(training_path, 'val_scores_maps_arr.h5')).astype(np.uint8),
-    load_from_h5(os.path.join(training_path, 'val_global_label_arr.h5')).astype(np.uint8)
-]
+    # for i in range(db[0].shape[0]):
+    #     label = str(db[1][i])
+    #     img = db[0][i].squeeze()
+    #     plt.imshow(img)
+    #     plt.title(label)
+    #     plt.show()
+    #     plt.pause(1e-3)
+    nb_epochs = 100
+    batch_size = 100
+    # model = build_model(nb_epochs)
+    model = build_model_vgg16_based(nb_epochs)
+    model_name = 'global_vgg16'
+    model.summary()
+    model_file_name = 'ptx_model_' + model_name + '.hdf5'
 
-# for i in range(db[0].shape[0]):
-#     label = str(db[1][i])
-#     img = db[0][i].squeeze()
-#     plt.imshow(img)
-#     plt.title(label)
-#     plt.show()
-#     plt.pause(1e-3)
-nb_epochs = 100
-batch_size = 100
-model = build_model(nb_epochs)
-model_name = 'global'
-model.summary()
-model_file_name = 'ptx_model_' + model_name + '.hdf5'
+    model_checkpoint = ModelCheckpoint(model_file_name, monitor='val_loss', mode='min', save_best_only=True, verbose=1)
+    early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=10, verbose=1)
+    reduce_lr_on_plateu = ReduceLROnPlateau(monitor='val_loss', mode='min', patience=5, factor=0.3, verbose=1)
+    plot_curves_callback = PlotLearningCurves()
+    callbacks = [model_checkpoint, early_stopping, reduce_lr_on_plateu, plot_curves_callback]
+    print('Start fitting...')
+    model.fit(db[0], db[1], batch_size=batch_size, epochs=nb_epochs,
+              validation_data=(db[2], db[3]),
+              verbose=1, shuffle=True,
+              callbacks=callbacks)
 
-model_checkpoint = ModelCheckpoint(model_file_name, monitor='val_loss', mode='min', save_best_only=True, verbose=1)
-early_stopping = EarlyStopping(monitor='val_loss', mode='min', patience=10, verbose=1)
-reduce_lr_on_plateu = ReduceLROnPlateau(monitor='val_loss', mode='min', patience=5, factor=0.3, verbose=1)
-plot_curves_callback = PlotLearningCurves()
-callbacks = [model_checkpoint, early_stopping, reduce_lr_on_plateu, plot_curves_callback]
-print('Start fitting...')
-model.fit(db[0], db[1], batch_size=batch_size, epochs=nb_epochs,
-          validation_data=(db[2], db[3]),
-          verbose=1, shuffle=True,
-          callbacks=callbacks)
+    print("Done!")
 
-print("Done!")
+
+if __name__ == '__main__':
+    train_model()
