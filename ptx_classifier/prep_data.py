4b1fa08839bdacbe11c88ce8eddf34f96db5fb1d
avielbl <avielbl@gmail.com>
2018-01-19 14:15:11 +0200

    changed prep_data script to handle both train and test sets

--- ptx_classifier/prep_data.py
+++ ptx_classifier/prep_data.py
@@ -3,20 +3,28 @@ from shutil import copy2
 import os
 import numpy as np
 
-copy_root = r'C:\projects\CXR_thesis\data_repo\TRAINING'
+def prep_set(root):
+    right_cases = getimages(['r_ptx'], ['PA', 'AP'])
+    left_cases = getimages(['l_ptx'], ['PA', 'AP'])
+    pos_path = os.path.join(root, 'pos_cases')
+    right_path = os.path.join(pos_path, 'right')
+    left_path = os.path.join(pos_path, 'left')
+    nb_pos = len(right_cases) + len(left_cases)
+    for case in right_cases:
+        copy2(case, right_path)
+    for case in left_cases:
+        copy2(case, left_path)
 
-pos_cases = getimages(['r_ptx', 'l_ptx'], ['PA', 'AP'])
-pos_path = os.path.join(copy_root, 'pos_cases')
+    neg_cases = getimages(['r_ptx', 'l_ptx'], ['PA', 'AP'], getneg=True)
+    neg_path = os.path.join(root, 'neg_cases')
 
-for case in pos_cases:
-    copy2(case, pos_path)
+    neg_samples = np.random.choice(len(neg_cases), size=np.uint16(1.1*nb_pos), replace=False)
 
-neg_cases = getimages(['r_ptx', 'l_ptx'], ['PA', 'AP'], getneg=True)
-neg_path = os.path.join(copy_root, 'neg_cases')
+    sampled_neg_cases = [neg_cases[i] for i in neg_samples]
+    for i, case in enumerate(sampled_neg_cases):
+        copy2(case, neg_path)
+        print('copied {}/{}'.format(i, len(sampled_neg_cases)))
 
-neg_samples = np.random.choice(len(neg_cases), size=np.uint16(1.1*len(pos_cases)), replace=False)
-
-sampled_neg_cases = [neg_cases[i] for i in neg_samples]
-for i, case in enumerate(sampled_neg_cases):
-    copy2(case, neg_path)
-    print('copied {}/{}'.format(i, len(sampled_neg_cases)))
+if __name__ == '__main__':
+    # prep_set(r'C:\projects\CXR_thesis\data_repo\TRAINING')
+    prep_set(r'C:\projects\CXR_thesis\data_repo\TEST')
