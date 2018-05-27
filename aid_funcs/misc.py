ad1bfad6905f6c83a7cd826a6558a91c25bec4c4
avibenco <avibenco@gmail.com>
2018-05-27 07:49:11 +0300

    Adds progress bar print function

--- aid_funcs/misc.py
+++ aid_funcs/misc.py
@@ -1,5 +1,7 @@
 import gzip
 import pickle
+import sys
+
 import h5py
 import numpy as np
 import io
@@ -198,3 +200,46 @@ def roc_plotter(gt, scores, title, plot_thresh=False):
     with open(title + '_roc.pkl', 'wb') as f:
         pickle.dump((fpr, tpr, opt_ind), f)
     return fig
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix='', suffix='', decimals=1, bar_length=100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        bar_length  - Optional  : character length of bar (Int)
+
+    # Sample Usage:
+
+
+    from time import sleep
+
+    # A List of Items
+    items = list(range(0, 57))
+    l = len(items)
+
+    # Initial call to print 0% progress
+    printProgressBar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+    for i, item in enumerate(items):
+        # Do stuff...
+        sleep(0.1)
+        # Update Progress Bar
+        printProgressBar(i + 1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+    # Sample Output:
+    Progress: |█████████████████████████████████████████████-----| 90.0% Complete
+    """
+    str_format = "{0:." + str(decimals) + "f}"
+    percents = str_format.format(100 * (iteration / float(total)))
+    filled_length = int(round(bar_length * iteration / float(total)))
+    bar = '█' * filled_length + '-' * (bar_length - filled_length)
+
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
+
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
