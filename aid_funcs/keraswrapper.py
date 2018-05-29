b8679102f1e43f9b6f8314c4d16c44eb8224a88f
Aviel Blumenfeld <36601405+AvielBlumenfeld@users.noreply.github.com>
2018-05-29 22:43:45 +0300

    adds focal_loss function

--- aid_funcs/keraswrapper.py
+++ aid_funcs/keraswrapper.py
@@ -44,6 +44,16 @@ def load_model(model_path, custom_objects=None):
         return keras_load_model(model_path, custom_objects=custom_objects)
 
 
+def focal_loss(gamma=2., alpha=.25):
+    def focal_loss_fixed(y_true, y_pred):
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.sum(
+                (1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
+
+    return focal_loss_fixed
+
+
 def dice_coef(y_true, y_pred):
     '''
     Computes dice cooeficient based on Sørensen's formula with smooth factor to avoid devision by zero
